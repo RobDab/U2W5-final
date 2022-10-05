73d1472d8283a52afd7f0594a9e665194fbb8cb9
RobDab <roberto.dabb@gmail.com>
2022-10-05 11:33:14 +0200

    started again

---
+++ script.js
@@ -0,0 +1,26 @@
+// quando clicco su una carta viene "selezionata"
+// quajddo clicco su un'altra viene selezionata e confrontata con la prima
+
+// se sono uguali restano selezionate ed evidenziate e non sono più cliccabili
+
+let cards = document.getElementsByClassName('card')
+console.log(cards)
+
+let FIRST_CARTD
+let SECOND_CARD
+
+
+
+function flip(userClick){
+    console.log(userClick.target)
+    userClick.target.classList.add('flipped')
+
+    if(FIRST_CARD === undefined){
+        FIRST_CARD = userClick.target
+    }else{
+        SECOND_CARD = userClick.target
+        for (let card of cards) {
+            card.removeAttribute('onclick')
+        }
+    }
+}
